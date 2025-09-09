ed19f44bf6146009956c5bec55c17171c5615c0f
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-09 19:41:11 -0300

    feat: add application components and update styles with Tailwind CSS

---
+++ apps/application/src/app/(application)/page.tsx
@@ -0,0 +1,12 @@
+const ApplicationPage = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-2xl font-bold">Application Page</h1>
+        <p>Welcome to the Application Page!</p>
+      </div>
+    </div>
+  );
+};
+
+export default ApplicationPage;
