1d86e2e2d9ad0a4528eca52be75442af50d0bc54
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-10 09:08:15 -0300

    feat: update ApplicationPage to include Header component for improved layout

--- apps/application/src/app/(application)/page.tsx
+++ apps/application/src/app/(application)/page.tsx
@@ -1,7 +1,18 @@
+import { Header } from "@/components/header";
+
 const ApplicationPage = () => {
   return (
     <div>
-      <div>
+      <Header
+        title="Dashboard"
+        subtitle="Visualização geral do sistema"
+        content={
+          <>
+            <div className="h-8 w-8 rounded-full bg-gray-300" />
+          </>
+        }
+      />
+      <div className="p-4 m-4">
         <h1 className="text-2xl font-bold">Application Page</h1>
         <p>Welcome to the Application Page!</p>
       </div>
