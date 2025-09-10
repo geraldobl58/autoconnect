22300f7848475058f70b7054ea51eae45a37e1e1
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-10 11:37:07 -0300

    feat: enable CORS and add Swagger documentation setup in main application bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Autoconnect API')
+    .setDescription('The Autoconnect API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, doc);
+
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
